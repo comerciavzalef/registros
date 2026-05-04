72b8feb18fdf7f1ba5f8726fe0662fb18aa93774
comerciavzalef <comercialvz.alef@gmail.com>
2026-05-04 14:38:22 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    REQUISIÇÕES DIGITAL — SERVICE WORKER v1.0
    ============================================================ */
 
-var CACHE_NAME = 'requisicoes-v5';
+var CACHE_NAME = 'requisicoes-v5.1';
 var ASSETS = [
     './',
     './index.html',
